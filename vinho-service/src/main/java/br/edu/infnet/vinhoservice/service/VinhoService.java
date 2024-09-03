a02bbf4d0fd4f80694a340703bd26115e7b09011
pedronora <pedro@nora.vc>
2024-09-03 11:21:16 -0300

    Criado VinhoService

---
+++ vinho-service/src/main/java/br/edu/infnet/vinhoservice/service/VinhoService.java
@@ -0,0 +1,35 @@
+package br.edu.infnet.vinhoservice.service;
+
+import br.edu.infnet.vinhoservice.model.Vinho;
+import br.edu.infnet.vinhoservice.repository.VinhoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class VinhoService {
+    private final VinhoRepository vinhoRepository;
+
+    public Vinho create(Vinho vinho) {
+        return vinhoRepository.save(vinho);
+    }
+
+    public List<Vinho> findAll() {
+        return vinhoRepository.findAll();
+    }
+
+    public Optional<Vinho> findById(Long id) {
+        return vinhoRepository.findById(id);
+    }
+
+    public Vinho update(Vinho vinho) {
+        return vinhoRepository.save(vinho);
+    }
+
+    public void deleteById(Long id) {
+        vinhoRepository.deleteById(id);
+    }
+}
