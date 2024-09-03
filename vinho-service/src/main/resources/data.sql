9d81f3f92bca7b4065f02e96d57070da0cab0d01
pedronora <pedro@nora.vc>
2024-09-03 18:01:23 -0300

    SQL para introduzir valores no BD ao iniciar o sistema

---
+++ vinho-service/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO vinho (nome, preco, uva, docura, cor, vinicola_id)
+VALUES ('Vinho do Porto', 45.99, 'Touriga Nacional', 'SECO', 'TINTO', 1),
+       ('Chardonnay Reserva', 75.50, 'Chardonnay', 'DEMISEC', 'BRANCO', 2),
+       ('Merlot Clássico', 60.00, 'Merlot', 'SECO', 'TINTO', 3),
+       ('Rosé Provence', 50.25, 'Grenache', 'DOCE', 'ROSE', 4),
+       ('Cabernet Sauvignon Premium', 120.99, 'Cabernet Sauvignon', 'SECO',
+        'TINTO', 5);
