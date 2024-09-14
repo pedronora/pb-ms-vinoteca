aa85338fbaa8d6ea2e8763a5199551a8f1612aa1
cirosouza <ciro_lut@hotmail.com>
2024-09-14 10:18:39 -0300

    Ajuste de configuração na classe nota-fiscal-worker

--- notafiscal-worker/src/main/java/br/edu/infnet/notafiscalworker/model/Pedido.java
+++ notafiscal-worker/src/main/java/br/edu/infnet/notafiscalworker/model/Pedido.java
@@ -1,22 +1,19 @@
 package br.edu.infnet.notafiscalworker.model;
 
-import br.edu.infnet.pedidoservice.model.ItemPedido;
+
 import java.math.BigDecimal;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Document(collection = "pedidos")
 public class Pedido {
-  @Id private String id;
+  private String id;
   private List<ItemPedido> items;
   private BigDecimal totalImposto;
 }
